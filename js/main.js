5ec48524b887b42869c4c289ee75e44f74735b7f
Sepehr Samarzadeh Vajdehfar <ssamarzadeh@gmail.com>
2023-01-08 21:41:39 +0330

    JS is added

---
+++ js/main.js
@@ -0,0 +1,160 @@
+const initApp=()=>{
+    const currentValueElem=document.querySelector(".currentvalue");
+    const previousValueElm=document.querySelector(".previousvalue");
+    let itemArray=[];
+    const equationArray=[];
+    let newNumberFlag=false;
+
+
+    const inputButtons=document.querySelectorAll(".number");
+    inputButtons.forEach(button=>{
+        button.addEventListener("click",(event)=>{
+
+            const newInput=event.target.textContent;
+            if(newNumberFlag){
+                currentValueElem.value=newInput;
+                newNumberFlag=false;
+            }else{
+                currentValueElem.value=
+                currentValueElem.value==0
+                ? newInput
+                : `${currentValueElem.value}${newInput}`;
+            }
+
+        });
+    });
+
+
+
+    const opButtons=document.querySelectorAll(".operator");
+    opButtons.forEach(button=>{
+        button.addEventListener("click",(event)=>{
+
+            //equal sign showing
+            if(newNumberFlag){
+                previousValueElm.textContent="";
+                itemArray=[];
+            }
+
+            const newOperator=event.target.textContent;
+            const currentVal=currentValueElem.value;
+
+            //need number first
+            if(!itemArray.length && currentVal==0) return;
+
+            //begin new equation
+            if(!itemArray.length){
+                itemArray.push(currentVal,newOperator);
+                previousValueElm.textContent=
+                `${currentVal}
+                 ${newOperator}`;
+                 return newNumberFlag=true;
+            }
+
+            //complete equation
+            if(itemArray.length){
+
+                itemArray.push(currentVal);//3rd element
+
+                const equationObj={
+                    num1:parseFloat(itemArray[0]),
+                    num2:parseFloat(currentVal),
+                    op:itemArray[1],
+                }
+                equationArray.push(equationObj);
+                const equationString=
+                `${equationObj['num1']}
+                ${equationObj['op']}
+                ${equationObj['num2']}`;
+
+
+                const newValue=calculate(equationString,currentValueElem);
+
+                previousValueElm.textContent=`${newValue} ${newOperator}`
+
+                //start new equation
+                itemArray=[newValue,newOperator];
+                newNumberFlag=true;
+                console.log(equationArray);
+
+            }
+
+
+
+        });
+    });
+
+
+    const equalsBtton=document.querySelector(".equals");
+    equalsBtton.addEventListener("click",()=>{
+        const currentVal=currentValueElem.value;
+        let equationObj;
+
+        //pressing equals continuously
+        if(!itemArray.length && equationArray.length){
+            const lastEquation=equationArray[equationArray.length-1];
+            equationObj={
+                num1:parseFloat(currentVal),
+                num2:lastEquation.num2,
+                op:lastEquation.op
+            }
+        }else if(!itemArray.length){
+            return currentVal;
+        }else{
+            itemArray.push(currentVal);
+            equationObj={
+                num1:parseFloat(itemArray[0]),
+                num2:parseFloat(currentVal),
+                op:itemArray[1]
+            }
+        }
+
+        equationArray.push(equationObj);
+        
+        const equationString=`${equationObj['num1']} ${equationObj['op']} ${equationObj['num2']}`;
+
+        calculate(equationString,currentValueElem);
+
+        previousValueElm.textContent=`${equationString} =`;
+
+        newNumberFlag=true;
+        itemArray=[];
+        console.log(equationArray);
+
+    });
+
+
+    const clearButtons=document.querySelectorAll(".clear, .clearEntry");
+    clearButtons.forEach(button=>{
+        button.addEventListener("click",(event)=>{
+            currentValueElem.value=0;
+            if(event.target.classList.contains('clear')){
+                previousValueElm.textContent='';
+                itemArray=[];
+            }
+        });
+    });
+
+    const deleteButton=document.querySelector(".delete");
+    deleteButton.addEventListener("click",()=>{
+        currentValueElem.value=currentValueElem.value.slice(0,-1);
+    });
+
+    const signChangeButton=document.querySelector(".signChange");
+    signChangeButton.addEventListener("click",()=>{
+        currentValueElem.value=parseFloat(currentValueElem.value)*-1
+    })
+
+
+}
+
+document.addEventListener("DOMContentLoaded",initApp);
+
+const calculate=(equation,currentValueElem)=>{
+    const regex=/(^[*/=])|(\s)/g;
+    equation.replace(regex,'');
+    const divByZero=/(\/0)/.test(equation);
+    if(divByZero)return currentValueElem.value=0;
+    return currentValueElem.value=eval(equation);
+
+}
